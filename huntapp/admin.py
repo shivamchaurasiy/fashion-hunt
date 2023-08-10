e5e30f780342b500219bca91899a332dada512e9
shivamchaurasiy <129745697+shivamchaurasiy@users.noreply.github.com>
2023-08-10 21:00:21 +0530

    Delete huntapp directory

--- huntapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from huntapp.models import Category
-
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display=['cid', 'cname']
--- huntapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HuntappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'huntapp'
